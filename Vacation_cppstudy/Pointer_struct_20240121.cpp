fc5c70b401da32e287f6905f611367389c69e366
wonhyekyeong <hkw_61@naver.com>
2024-01-22 23:40:16 +0900

    CPP study with book
    ch.12-13 (array, struct, pointer, and other compount types)

---
+++ Vacation_cppstudy/Pointer_struct_20240121.cpp
@@ -0,0 +1,30 @@
+﻿// 구조체를 가리키는 포인터
+
+
+#include<iostream>
+using namespace std;
+
+struct Dizzy
+{
+	int id; //구조체 변수의 고유값
+	Dizzy* p; //Dizzy 구조체를 가리키는 포인터
+};
+
+int main()
+{
+	//Dizzy 객체를 3개 만들고, 서로를 가리키도록 함
+	Dizzy a, b, c;
+	a.id = 1;
+	a.p = &b;
+	b.id = 2;
+	b.p = &c;
+	c.id = 3;
+	c.p = &a;
+
+	//a만을 이용해서 a,b,c의 모든 값에 접근한다.
+	//링크드 리스트 형태 (ch.19)
+	cout << "a.id = " << a.id << endl;
+	cout << "b.id = " << a.p->id << endl;
+	cout << "c.id = " << a.p->p->id << endl;
+	cout << "a.id = " << a.p->p->id << endl;
+}
---
+++ Vacation_cppstudy/Pointer_struct_linkedlist_20240121.cpp
@@ -0,0 +1,21 @@
+﻿// 구조체를 가리키는 포인터
+
+
+#include<iostream>
+using namespace std;
+
+struct Point
+{
+	int x, y;
+};
+
+int main() 
+{
+	Point pt[3] = { {100,100}, {30,50} {80,20}};
+	Point* p[5] = { &pt[0], &pt[1], &pt[2], &pt[0], &pt[0], &pt[1]};
+
+	for (int i = 0; i < 5; i++)
+	{
+		cout << p[i] << " and " << *p[i] << endl;
+	}
+}
