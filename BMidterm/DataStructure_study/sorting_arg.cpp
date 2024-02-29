415ea2d8e359dccfe5ef9611f10a26e58fba9dc6
wonhyekyeong <hkw_61@naver.com>
2024-02-29 23:23:12 +0900

    CPP Data Structure study commit
    ch.1 sorting argorithm

---
+++ BMidterm/DataStructure_study/sorting_arg.cpp
@@ -0,0 +1,20 @@
+//선택 정렬 알고리즘 함수 coding - C++ 자료구조론(2nd Edition) p.22
+
+#include<iostream>
+#include<array>
+using namespace std;
+
+void SelectionSort(int* a, const int n)
+{
+	//최초의 값을 입력 받을 임시변수 temp 선언
+	int temp = a[0];
+	for (int i = 1; i < n; i++)
+	{
+		
+	}
+}
+
+int main()
+{
+
+}
