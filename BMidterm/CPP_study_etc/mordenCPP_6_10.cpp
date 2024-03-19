ef4580766cb5e14b4dbef491a239d7992b5a5748
wonhyekyeong <hkw_61@naver.com>
2024-03-19 17:18:58 +0900

    CPP study etc commit 20240319
    modern C++ challenge #6-10

---
+++ BMidterm/CPP_study_etc/mordenCPP_6_10.cpp
@@ -0,0 +1,43 @@
+//modern C++ challenge - #6~10
+
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+//#1. 과잉수 구하기 : 과잉수란 자신을 제외한 약수들의 합이 자신보다 작은 수를 이야기한다.
+//과잉수를 벡터1에 넣고, 과잉수의 과잉을 벡터2에 넣어서 저장한다. 출력시 이 둘을 출력하도록 한다.
+void iAbundance(int const iNum)
+{
+	vector<int> vec1;	//과잉수 저장
+	vector<int> vec2;	//과잉수의 과잉 저장.
+
+	if (iNum < 12)
+	{
+		cout << "There are no abundance in this range.";
+	}
+	else
+	{
+		for (int i = 12; i <= iNum; i++)
+		{
+			int iTotal = 1;
+			for (int j = 2; j < i; j++)	//약수인지 check.
+			{
+				if (iTotal % j == 0)
+				{
+					iTotal += j;
+				}
+			}
+
+			if (iTotal > i)
+			{
+				vec1.push_back(i);
+				vec2.push_back(iTotal);
+			}
+		}
+		for (int i = 0; i < vec1.size(); i++)
+		{
+			cout << "Abundance number(" << vec1[i] << ") : " << "abundance(" << vec2[i] <<")" << endl;
+		}
+	}
+}
