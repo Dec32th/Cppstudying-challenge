983c2f90b10e08e900dceef34cb87b3a33f16e0e
wonhyekyeong <hkw_61@naver.com>
2024-03-26 11:53:25 +0900

    CPP study etc commit 20240326
    modern C++ challenge #11-15

--- BMidterm/CPP_study_etc/modernCPP_11_15.cpp
+++ BMidterm/CPP_study_etc/modernCPP_11_15.cpp
@@ -3,6 +3,7 @@
 #include<sstream>
 #include<vector>
 #include<utility>
+#include<random>
 
 using namespace std;
 
@@ -34,58 +35,127 @@ string sToRomain(int iNum)
 
 //#12. 가장 긴 콜라츠 수열 추측하기 - 출력
 //백만 이하의 숫자중, 변환을 통해서 가장 많이 연산을 해서 변환된 수를 찾고, 그 수의 연산과정을 출력하면 된다..
-//1부터 100만까지 다 해야하므로 함수가 존나 무거움.
-void vCollatz(unsigned int iLimit)
+//1부터 100만까지 다 해야하므로 함수가 무거움.
+// 
+//void vCollatz(unsigned int iLimit)
+//{
+//	vector<pair<int, long long>> vec1;
+//	int iCount = 0;
+//	for (int i = 1; i <= iLimit; i++)
+//	{
+//		if (i == 1)
+//			vec1.push_back((make_pair(i, iCount)));
+//		while (i != 1)
+//		{
+//			if (i % 2 == 0)
+//			{
+//				i /= 2;
+//				iCount++;
+//			}
+//			else
+//			{
+//				i = i * 3 + 1;
+//				iCount++;
+//			}
+//		}
+//		
+//	}
+//	int iResult1 = vec1[0].first;
+//	long long iResult2 = vec1[0].second;
+//	for (auto elem : vec1)
+//	{
+//		if (iResult2 < elem.second)
+//		{
+//			iResult2 = elem.second;
+//			iResult1 = elem.first;
+//		}
+//			
+//	}
+//	
+//	int iNumber1 = iResult1;
+//	while (iNumber1 != 0)
+//	{
+//			if (iNumber1 % 2 == 0)
+//			{
+//				cout << iNumber1 <<endl;
+//				iNumber1 /= 2;
+//				
+//			}
+//			else
+//			{
+//				cout << iNumber1 << endl;
+//				iNumber1 = iNumber1 * 3 + 1;
+//			}
+//	}
+//	cout << iResult2;
+//}
+
+pair<unsigned long long, long>longest_collatz(unsigned long long const limit)
 {
-	vector<pair<int, long long>> vec1;
-	int iCount = 0;
-	for (int i = 1; i <= iLimit; i++)
+	long length = 0;
+	unsigned long long number = 0;
+	vector<int> cache(limit + 1, 0);
+	for (unsigned long long i = 2; i <= limit; i++)
 	{
-		if (i == 1)
-			vec1.push_back((make_pair(i, iCount)));
-		while (i != 1)
+		auto n = i;
+		long steps = 0;
+		while (n != 1 && n >= i)
 		{
-			if (i % 2 == 0)
-			{
-				i /= 2;
-				iCount++;
-			}
+			if ((n % 2) == 0)
+				n = n / 2;
 			else
-			{
-				i = i * 3 + 1;
-				iCount++;
-			}
+				n = n * 3 + 1;
+			steps++;
 		}
-		
-	}
-	int iResult1 = vec1[0].first;
-	long long iResult2 = vec1[0].second;
-	for (auto elem : vec1)
-	{
-		if (iResult2 < elem.second)
+		cache[i] = steps + cache[n];
+
+		if (cache[i] > length)
 		{
-			iResult2 = elem.second;
-			iResult1 = elem.first;
+			length = cache[i];
+			number += i;
 		}
-			
 	}
+	return make_pair(number, length);
+}
+
+//#13 파이 값 계산하기 
+//책에서의 방법과 조금 다르게 써본다. 
+// 여기서는 그레고리 라이프니츠 급수를 사용 - 20240326기준, 제대로 동작은 하나, 
+//while문을 너무 오랫동안 돌아서 거의 무한루프급으로 시간이 오래 걸리는 듯 하다.
+double dcompute_pi()
+{
+	double result = 4.;
+	int iCount = 1;
+
+	do
+	{
+		if (iCount % 2 != 0)
+		{
+			result -= 4. / ((2 * iCount) + 1);
+			cout << result << endl;
+			iCount++;
+			cout << iCount << endl;
+		}
+		else
+		{
+			result += 4. / ((2 * iCount) + 1);
+			cout << result << endl;
+			iCount++;
+			cout << iCount << endl;
+		}
+
+	} while (result != 3.1415926535);
+	return result;
+}
+
+
+//#13 책에 적혀있는 방법 사용
+template<typename E = std::mt19937,
+	typename D = std::uniform_real_distribution<>>
+	double compute_pi(E& engine, D& dist, int const samples = 1000000)
+{
+	auto hit = 0;
 	
-	int iNumber1 = iResult1;
-	while (iNumber1 != 0)
-	{
-			if (iNumber1 % 2 == 0)
-			{
-				cout << iNumber1 <<endl;
-				iNumber1 /= 2;
-				
-			}
-			else
-			{
-				cout << iNumber1 << endl;
-				iNumber1 = iNumber1 * 3 + 1;
-			}
-	}
-	cout << iResult2;
 }
 
 int main()
@@ -93,6 +163,6 @@ int main()
 	/*cout << sToRomain(53) << endl;
 	cout << sToRomain(83) << endl;
 	cout << sToRomain(901) << endl;*/
-
-
+	
+	cout << dcompute_pi();
 }
