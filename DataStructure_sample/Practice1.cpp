21dd89dd0f46a57bd07fc8ce7158d8fd2debb3cd
wonhyekyeong <hkw_61@naver.com>
2024-04-01 19:24:29 +0900

    CPP data structure commit
    sample code - 20240401

---
+++ DataStructure_sample/Practice1.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<array>
+
+using namespace std;
+//array에서 찾고, sizeOfArray가 배열의 크기, value값을 찾고 없을 경우에는 -1
+int BinarySearch(int array[], int sizeOfArray, int value)
+{
+	int midPoint = sizeOfArray/2;
+	int First = 0;
+	int Last = sizeOfArray - 1;
+	
+
+	bool bChecker = false;
+
+	while (First != Last && bChecker)
+	{
+		return 0;
+	}
+	return 0;
+}
